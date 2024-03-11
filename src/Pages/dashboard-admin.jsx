40b88f6e8f46663e80374e3bde788d3c4b2855fb
Harsh Gupta <harshgupta0028@gmail.com>
2024-03-11 21:41:28 +0530

    first commit

---
+++ src/Pages/dashboard-admin.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+
+import "../Styles/dashboard-admin.css";
+
+function DashboardAdmin() {
+  const [option, setOption] = useState(0);
+
+  const transaction = () => {
+    return (
+      <table className="table_admin">
+        <tbody>
+          <tr onClick={()=>setOption(2)}>
+            <th>Blood bank name</th>
+            <th>Unique ID(UID)</th>
+          </tr>
+          <tr onClick={()=>setOption(2)}>
+            <td>ABC blood bank</td>
+            <td>3601</td>
+          </tr>
+          <tr onClick={()=>setOption(2)}>
+            <td>Healthy blood bank</td>
+            <td>9087</td>
+          </tr>
+          <tr onClick={()=>setOption(2)}>
+            <td>Test ..</td>
+            <td>6589</td>
+          </tr>
+  
+        </tbody>
+      </table>
+    );
+  };
+  
+  const availability = () => {
+    return(
+      <table className="table_admin">
+        <tbody>
+          <tr>
+            <th>Blood bank name</th>
+            <th>A+(in ltr.)</th>
+            <th>B+(in ltr.)</th>
+            <th>AB+(in ltr.)</th>
+            <th>O+(in ltr.)</th>
+            <th>A-(in ltr.)</th>
+            <th>B-(in ltr.)</th>
+            <th>AB-(in ltr.)</th>
+            <th>O-(in ltr.)</th>
+          </tr>
+          <tr>
+            <td>ABC</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        
+          <tr>
+            <td>Healthy Blood bank</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Test ..</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  };
+  
+  const transaction_details = ()=>{
+    return(
+      <table className="table_admin">
+        <tbody>
+          <tr>
+            
+            <th>Transations</th>
+          </tr>
+          <tr>
+            <td>ABC</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+
+  return (
+    <div className="main_admin">
+      <div className="left_nav_admin">
+        <div className="blood_bank_name_admin">BloodChain</div>
+        <div className="navigation_list_admin">
+          <div
+            className={`nav_option_admin ${option == 0 ? "nav_option_selected_admin" : ""}`}
+            onClick={() => {
+              setOption(0);
+            }}
+          >
+            <li>Blood bank data(Transactions)</li>
+          </div>
+          <div
+            className={`nav_option_admin ${option == 1 ? "nav_option_selected_admin" : ""}`}
+            onClick={() => {
+              setOption(1);
+            }}
+          >
+            <li>Blood bank data(Availability) </li>
+          </div>
+         
+        </div>
+        <div className="req_btn_admin">
+          <button type="submit">Request for blood</button>
+        </div>
+      </div>
+      <div className="right_display_admin">
+        {option === 0
+          ? transaction()
+          : option === 1
+          ? availability()
+          : option===2
+          ? transaction_details()
+          :null}
+      </div>
+    </div>
+  );
+}
+
+export default DashboardAdmin;
---
+++ src/Pages/login.jsx
@@ -0,0 +1,40 @@
+import React,{useContext} from "react";
+import "../Styles/login.css";
+import imgimg from "../assets/img_login.png";
+import { DemoContext } from "../contexts/democontext";
+
+function Login() {
+  const { contracts, setContracts } = useContext(DemoContext);
+  const { account, setAccount } = useContext(DemoContext);
+  return (
+    <div className="main">
+      <div className="left_div">
+        <img src={imgimg} alt="Login Image" />
+      </div>
+      <div className="right_div">
+        <div className="innerRightDiv">
+          <div className="heading">Welcome Back!</div>
+          <div className="subheading">Please enter your details to login</div>
+          <div className="form_div">
+            <div className="form_field">
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="form_field">
+              <input type="password" placeholder="Password" />
+            </div>
+          </div>
+          <div className="login_btn">
+            <button >Log in</button>
+          </div>
+          <div className="nav_like">
+            <p>Don't have an account?</p>
+            <a href="/register" 
+            >Register here now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
